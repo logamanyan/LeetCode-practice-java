0a2c6316b464188dd34dc30025fa46c82a243397
Logamanyan R <146450819+logamanyan@users.noreply.github.com>
2026-01-17 12:04:32 +0530

    Time: 32 ms (76.29%) | Memory: 45 MB (26.94%) - LeetSync

---
+++ 313-super-ugly-number/super-ugly-number.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int nthSuperUglyNumber(int n, int[] primes) {
+
+        int k = primes.length;
+
+        long[] ugly = new long[n];
+        ugly[0] = 1;
+
+        int[] index = new int[k];
+        long[] next = new long[k];
+
+        for (int i = 0; i < k; i++) {
+            next[i] = primes[i];
+        }
+
+        for (int i = 1; i < n; i++) {
+
+            long min = next[0];
+            for (int j = 1; j < k; j++) {
+                min = Math.min(min, next[j]);
+            }
+
+            ugly[i] = min;
+
+            for (int j = 0; j < k; j++) {
+                if (next[j] == min) {
+                    index[j]++;
+                    next[j] = ugly[index[j]] * primes[j];
+                }
+            }
+        }
+
+        return (int) ugly[n - 1];
+    }
+}
