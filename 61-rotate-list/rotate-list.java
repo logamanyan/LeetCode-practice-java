4b27bb3ad97f3c6d633ce6fbdc7f387326e44a5d
Logamanyan R <146450819+logamanyan@users.noreply.github.com>
2026-01-22 22:11:46 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (15.62%) - LeetSync

--- 61-rotate-list/rotate-list.java
+++ 61-rotate-list/rotate-list.java
@@ -1,39 +1,34 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        if(head==null || head.next==null || k==0) return head;
-        int n=1;
-        ListNode last = head;
-        while(last.next!=null){
-            n++;
-            last = last.next;
+
+        if (head == null || head.next == null || k == 0)
+            return head;
+
+        ListNode temp = head;
+        int l = 1;
+
+        // Find length
+        while (temp.next != null) {
+            temp = temp.next;
+            l++;
         }
 
-        k = k%n;
-        if(k==0) return head;
+        k = k % l;
+        if (k == 0)
+            return head;   // ✅ no cycle created
 
-        int c = n-k;
-        ListNode temp = head;
-        int count=1;
-        while(temp!=null){
-            if(count==c) break;
-            count++;
+        // Make circular
+        temp.next = head;
+
+        // Find new tail
+        for (int i = 0; i < l - k; i++) {
             temp = temp.next;
         }
 
-        last.next = head;
-        ListNode res = temp.next;
+        // Break cycle
+        head = temp.next;
         temp.next = null;
 
-        return res;
+        return head;
     }
 }
